0643147d57661c7dde51a9a652ff2234c85df79f
coderjcronin <coderjmc@gmail.com>
2025-01-12 14:51:56 -0500

    Passed Ch 2 L 2

---
+++ src/test_textnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+        node3 = TextNode("This is a new test", TextType.CODE, "https://google.com")
+        node4 = TextNode("This is a new test", TextType.CODE, "https://google.com")
+        self.assertEqual(node3, node4)
+
+        node5 = TextNode("This shoudl fail", TextType.NORMAL)
+        node6 = TextNode("", TextType.NORMAL)
+        self.assertNotEqual(node5, node6)
+
+        node7 = TextNode("This shoudl fail", TextType.NORMAL, " ")
+        self.assertNotEqual(node5, node7)
+
+        node9 = TextNode("This shoudl fail", TextType.BOLD)
+        self.assertNotEqual(node5, node9)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
