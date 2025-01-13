69511d179ebed2c9641b2d7f460dd6de5cb5b4a1
coderjcronin <coderjmc@gmail.com>
2025-01-13 15:21:34 -0500

    Passed Ch 2 L 5

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,9 +11,10 @@ class HtmlNode():
     
     def props_to_html(self):
         temp_string = ""
-        for prop in self.props:
-             temp_string += f" {prop}=\"{self.props[prop]}\""
-        
+        if self.props != None:
+            for prop in self.props:
+                temp_string += f" {prop}=\"{self.props[prop]}\""
+
         return temp_string
     
     def __repr__(self):
@@ -30,3 +31,20 @@ class LeafNode(HtmlNode):
             return f"{self.value}"
         else:
             return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        
+class ParentNode(HtmlNode):
+    def __init__(self, tag, children, props = None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("ParentNode has no value")
+        elif self.children == None:
+            raise ValueError("ParentNode has no children.")
+        else:
+            new_string = f"<{self.tag}>"
+            for tag in self.children:
+                new_string += tag.to_html()
+            new_string += f"</{self.tag}>"
+
+            return new_string
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HtmlNode, LeafNode
+from htmlnode import HtmlNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_content(self):
@@ -11,19 +11,59 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(' href="http://coder-j.net"', htmlnode2.props_to_html())
 
 class TestLeafNode(unittest.TestCase):
-    def test_content(self):
-        
-        test_string = "This is a test string"
+    def test_error(self):
         try:
             leafnode1 = LeafNode()
         except Exception as e:
             self.assertRaises(TypeError, e)
+
+        try:
+            leafnode1 = LeafNode('Test')
+            leafnode1.value = None
+            leafnode1.to_html()
+        except ValueError as e:
+            self.assertEqual(type(e), ValueError)
+        else:
+            self.fail("LeafNode value set to None does not raise ValueError")
+
+    def test_content(self):
+        test_string = "This is a test string"
 
         leafnode2 = LeafNode(test_string)
         self.assertEqual("This is a test string", leafnode2.to_html())
 
         leafnode3 = LeafNode(test_string, 'a', { 'href' : 'http://coder-j.net' })
         self.assertEqual('<a href="http://coder-j.net">This is a test string</a>', leafnode3.to_html())
+
+class TestParentNode(unittest.TestCase):
+    def test_error(self):
+        try:
+            parentnode1 = ParentNode()
+        except Exception as e:
+            self.assertRaises(TypeError, e)
+
+        try:
+            parentnode1 = ParentNode('p', LeafNode('Test'))
+            parentnode1.tag = None
+            parentnode1.to_html()
+        except ValueError as e:
+            self.assertEqual(type(e), ValueError)
+        else:
+            self.fail("ParentNode tag equal to None not raising ValueError")
+
+    def test_content(self):
+        test_string = "This is a test string"
+
+        parentnode2 = ParentNode('p', (LeafNode(test_string), LeafNode(test_string, 'b'), LeafNode(test_string), LeafNode(test_string, 'a', { 'href' : 'http://coder-j.net'})))
+
+        self.assertEqual(
+            f'<p>{test_string}<b>{test_string}</b>{test_string}<a href="http://coder-j.net">{test_string}</a></p>',
+            parentnode2.to_html()
+            )
+
+        
+
+        
 
 if __name__ == "__main__":
     unittest.main()
