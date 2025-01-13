61d586ba79896f92a680337a571634c3baf1a615
coderjcronin <coderjmc@gmail.com>
2025-01-13 14:35:49 -0500

    Passed Ch 2 L 4

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,15 @@ class HtmlNode():
     
     def __repr__(self):
         return f"{self.tag}\n - value {self.value}\n - children {self.children}\n - props {self.props}"
+    
+class LeafNode(HtmlNode):
+    def __init__(self, value, tag = None, props = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("LeafNode has no value.")
+        if self.tag == None:
+            return f"{self.value}"
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HtmlNode
+from htmlnode import HtmlNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_content(self):
@@ -9,6 +9,21 @@ class TestHTMLNode(unittest.TestCase):
 
         htmlnode2 = HtmlNode("h1", "Title", "", { 'href' : 'http://coder-j.net' })
         self.assertEqual(' href="http://coder-j.net"', htmlnode2.props_to_html())
+
+class TestLeafNode(unittest.TestCase):
+    def test_content(self):
+        
+        test_string = "This is a test string"
+        try:
+            leafnode1 = LeafNode()
+        except Exception as e:
+            self.assertRaises(TypeError, e)
+
+        leafnode2 = LeafNode(test_string)
+        self.assertEqual("This is a test string", leafnode2.to_html())
+
+        leafnode3 = LeafNode(test_string, 'a', { 'href' : 'http://coder-j.net' })
+        self.assertEqual('<a href="http://coder-j.net">This is a test string</a>', leafnode3.to_html())
 
 if __name__ == "__main__":
     unittest.main()
