be727e4023410730769a6a7c73fe5fbdd0ea7ad2
coderjcronin <coderjmc@gmail.com>
2025-01-14 16:34:12 -0500

    Passed Ch 3 L 6, refactored image and link splits to keep strings in order (whoops)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
+# Imports
 from textnode import *
 from htmlnode import *
 from extractions import extract_markdown_images, extract_markdown_links
 from split_nodes import split_nodes_delimiter, split_nodes_link, split_nodes_image
+
+# Globals
+PATTERNS = [
+    ( '**', TextType.BOLD),
+    ( '*', TextType.ITALIC),
+    ( '`', TextType.CODE)
+]
+
 
 def main():
     pass
@@ -22,5 +31,17 @@ def text_node_to_html_node(text_node):
             return LeafNode(text_node.text, 'a', { 'href' : text_node.url })
         case _:
             raise ValueError("TextNode text type does not match ENUM list")
+        
+def text_to_textnodes(text):
+    temp_node = [TextNode(text, TextType.TEXT)]
+
+    temp_node = split_nodes_image(temp_node)
+    temp_node = split_nodes_link(temp_node)
+
+    for delimiter, text_type in PATTERNS:
+        temp_node = split_nodes_delimiter(temp_node, delimiter, text_type)
+
+    return temp_node
+    
                 
 main()
--- src/test_main.py
+++ src/test_main.py
@@ -5,9 +5,11 @@ from textnode import TextNode, TextType
 from htmlnode import HtmlNode, LeafNode, ParentNode
 
 #Test Variables
-test_string = 'This is a test'
-test_url = 'https://arstechnica.com'
-test_image = 'https://cdn.arstechnica.net/wp-content/uploads/2025/01/GettyImages-1244585198.jpg'
+TEST_STRING = 'This is a test'
+TEST_URL = 'https://arstechnica.com'
+TEST_IMAGE = 'https://cdn.arstechnica.net/wp-content/uploads/2025/01/GettyImages-1244585198.jpg'
+TEST_STRING_FROM_ASSIGNMENT = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+TEST_STRING_STRIPPED = "This is text with an italic word and a code block and an obi wan image and a link"
 
 class test_textnode_to_html_conversion(unittest.TestCase):
     def test_error(self):
@@ -21,57 +23,71 @@ class test_textnode_to_html_conversion(unittest.TestCase):
             self.fail("text_node_to_html_node did not raise ValueError with invalid TextType ENUM")
     
     def test_text_conversion(self):
-        textnode1 = TextNode(test_string, TextType.TEXT)
+        textnode1 = TextNode(TEST_STRING, TextType.TEXT)
 
         #Test TextType.NORMAL (no tag)
         test1 = main.text_node_to_html_node(textnode1)
         self.assertIsInstance(test1, LeafNode, "text_node_to_html_node did not return a LeafNode object")
-        self.assertEqual(test_string, test1.value, "Test string with TextType.TEXT did not convert properly to LeafNode value")
+        self.assertEqual(TEST_STRING, test1.value, "Test string with TextType.TEXT did not convert properly to LeafNode value")
         self.assertIsNone(test1.tag, "TextType.TEXT should not convert with a tag (should be None)")
 
     def test_italic_conversion(self):
-        textnode2 = TextNode(test_string, TextType.ITALIC)
+        textnode2 = TextNode(TEST_STRING, TextType.ITALIC)
         
         #Test TextType.ITALIC (i tag)
         test2 = main.text_node_to_html_node(textnode2)
         self.assertIsInstance(test2, LeafNode, "text_node_to_html_node did not return a LeafNode object")
-        self.assertEqual(test_string, test2.value, "Test string with TextType.ITALIC did not convert properly to LeafNode value")
+        self.assertEqual(TEST_STRING, test2.value, "Test string with TextType.ITALIC did not convert properly to LeafNode value")
         self.assertEqual('i', test2.tag, "TextType.ITALIC did not convert with 'i' tag")
 
     def test_code_conversion(self):
-        textnode3 = TextNode(test_string, TextType.CODE)
+        textnode3 = TextNode(TEST_STRING, TextType.CODE)
         
         #Test TextType.CODE (code tag)
         test3 = main.text_node_to_html_node(textnode3)
         self.assertIsInstance(test3, LeafNode, "text_node_to_html_node did not return a LeafNode object")
-        self.assertEqual(test_string, test3.value, "Test string with TextType.CODE did not convert properly to LeafNode value")
+        self.assertEqual(TEST_STRING, test3.value, "Test string with TextType.CODE did not convert properly to LeafNode value")
         self.assertEqual('code', test3.tag, "TextType.CODE did not convert with 'code' tag")
 
     def test_bold_conversion(self):
-        textnode4 = TextNode(test_string, TextType.BOLD)
+        textnode4 = TextNode(TEST_STRING, TextType.BOLD)
 
         #Test TextType.BOLD (b tag)
         test4 = main.text_node_to_html_node(textnode4)
         self.assertIsInstance(test4, LeafNode, "text_node_to_html_node did not return a LeafNode object")
-        self.assertEqual(test_string, test4.value, "Test string with TextType.BOLD did not convert properly to LeafNode value")
+        self.assertEqual(TEST_STRING, test4.value, "Test string with TextType.BOLD did not convert properly to LeafNode value")
         self.assertEqual('b', test4.tag, "TextType.BOLD did not convert with 'b' tag")      
 
     def test_link_conversion(self):
-        textnode5 = TextNode(test_string, TextType.LINK, test_url)
+        textnode5 = TextNode(TEST_STRING, TextType.LINK, TEST_URL)
 
         #Test TextType.LINK (a tag)
         test5 = main.text_node_to_html_node(textnode5)
         self.assertIsInstance(test5, LeafNode, "text_node_to_html_node did not return a LeafNode object")
-        self.assertEqual(test_string, test5.value, "Test string with TextType.LINK did not convert properly to LeafNode value")
+        self.assertEqual(TEST_STRING, test5.value, "Test string with TextType.LINK did not convert properly to LeafNode value")
         self.assertEqual('a', test5.tag, "TextType.LINK did not convert with 'a' tag")
-        self.assertEqual({ 'href' : test_url }, test5.props, "TextType.LINK did not convert with prop of href and URL")
+        self.assertEqual({ 'href' : TEST_URL }, test5.props, "TextType.LINK did not convert with prop of href and URL")
 
     def test_image_conversion(self):
-        textnode6 = TextNode(test_string, TextType.IMAGE, test_image)
+        textnode6 = TextNode(TEST_STRING, TextType.IMAGE, TEST_IMAGE)
 
         #Test TextType.IMAGE (img tag)
         test6 = main.text_node_to_html_node(textnode6)
         self.assertIsInstance(test6, LeafNode, "text_node_to_html_node did not return a LeafNode object")
         self.assertEqual("", test6.value, "Test string with TextType.IMAGE did not convert properly to LeafNode value (should be empty string)")
         self.assertEqual('img', test6.tag, "TextType.IMAGE did not convert with 'img' tag")
-        self.assertEqual({ 'src' : test_image, 'alt' : test_string }, test6.props, "TextType.IMAGE did not convert with props of src and alt")
+        self.assertEqual({ 'src' : TEST_IMAGE, 'alt' : TEST_STRING }, test6.props, "TextType.IMAGE did not convert with props of src and alt")
+
+    def test_text_to_textnodes(self):
+        results = main.text_to_textnodes(TEST_STRING_FROM_ASSIGNMENT)
+        
+        reconstruct_string = ""
+
+        for result in results:
+            self.assertTrue(isinstance(result, TextNode), "Expected TextNode type")
+            self.assertTrue(result.text_type in TextType, "Expected TextNode text_type to be enumerated in TextType")
+            if result.text_type == TextType.IMAGE or result.text_type == TextType.LINK:
+                self.assertIsNotNone(result.url, "Expected results with type LINK or IMAGE to have URL, had None")
+            reconstruct_string += result.text
+
+        self.assertEqual(reconstruct_string, TEST_STRING_STRIPPED, "Expected reconstructed string from TextNodes")
